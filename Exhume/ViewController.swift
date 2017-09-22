4250f458ecac36769b7092e1b3cc28560d746051
Carl Wieland <cwieland@esri.com>
2017-09-22 09:10:11 -0400

    initial commit

---
+++ Exhume/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Exhume
+//
+//  Created by Carl Wieland on 9/22/17.
+//  Copyright © 2017 Datum Apps. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
